7797f1f5b5dea475a2017cea2f163085ccba1526
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2023-10-07 19:54:54 +0200

    untested ver. i wrote on the BKV

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
-<!DOCTYPE html>
 <html lang="hu">
     <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="A Nagy Enciklopédia">
+        <meta name="keywords" content="Github tests ...">
+        <meta name="author" content="BudayBoldi, kastascigo, Erik">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Enciklopédija</title>
         <link rel="stylesheet" href="./style.css">
         <script language="JScript" src="./script.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
     </head>
     <body>
-        <p ID="p1">read wiki.txt as file - php</p>
-        <p ID="p2">read wiki.txt as file - php</p>
+    <?php
+    $file = fopen("./wiki.txt", "r") or die("ERROR!");
+    
+    while(!feof($file)) { 
+        $line = fgets($file);
+
+        if($line == "") {
+            $sec += 1;
+        } else {
+            echo "<p ID='p" . $sec . "'>" . $line . "</p>";
+        }
+    }
+    
+    fclose($file);
+    ?>
     </body>
 </html>
