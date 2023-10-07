50b42338db5687a4b39cc76f581b8680a2331155
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2023-10-07 12:39:19 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <title>Enciklopédija</title>
+        <link rel="stylesheet" href="./style.css">
+        <script language="JScript" src="./script.js"></script>
+        <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <p ID="p1">read wiki.txt as file - php</p>
+        <p ID="p2">read wiki.txt as file - php</p>
+    </body>
+</html>
