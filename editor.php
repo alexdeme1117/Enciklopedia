647442a1bffd314edaeb60f86a6173b35da37aae
verébpapagáj <78073686+alexdeme1117@users.noreply.github.com>
2023-11-10 08:44:36 +0100

    Update editor.php

--- editor.php
+++ editor.php
@@ -1,8 +1,13 @@
 <html lang="hu">
 <head>
-  <title>A <?= $search ?> szerkesztése</title>
+  <title>A <?= $_GET["search"] ?> szerkesztése</title>
 </head>
 <body>
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $text = $_POST['text'];
+  }
+  ?>
   <form method="post" action="">
     <textarea name="edit" style="font-size:large">
       <?= $text ?>
