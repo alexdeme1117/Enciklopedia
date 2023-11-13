80169026a12bae54033432d162c9b7384a42b9af
verébpapagáj <78073686+alexdeme1117@users.noreply.github.com>
2023-11-13 10:35:11 +0100

    Update editor.php
    Need for Options ...

--- editor.php
+++ editor.php
@@ -1,17 +1,18 @@
 <html lang="hu">
 <head>
-  <title>A <?= $_GET["search"] ?> szerkesztése</title>
+  <title>A <?=$_GET["search"]?> szerkesztése</title>
 </head>
 <body>
   <?php
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $text = $_POST['text'];
+    $text = htmlspecialchars($_POST['text']);
+    $file = fopen("wiki.txt","w");
+    fwrite($file, $text);
+    fclose($file);
   }
   ?>
   <form method="post" action="">
-    <textarea name="edit" style="font-size:large">
-      <?= $text ?>
-    </textarea>
+    <textarea name="text" style="font-size:large"></textarea>
     <input type="submit" value="Szerkesztés"/>
   </form>
 </body>
